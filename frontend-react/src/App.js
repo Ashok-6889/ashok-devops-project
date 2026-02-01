7d8483b38dd9ed92466c7e65b5293337c1f76db3
Ashok-6889 <ashoknelluri2nd@gmail.com>
2026-02-01 20:06:04 +0530

    Update App.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -5,8 +5,10 @@ function App() {
   const [cart, setCart] = useState(0);
 
   const clap = () => {
-    if (cart + 1000 <= 100000) {
-      setCart(cart + 1000);
+    const amount = 1000;
+
+    if (cart + amount <= 100000) {
+      setCart(cart + amount);
     } else {
       alert("Cart limit reached (1,00,000)");
     }
@@ -15,7 +17,11 @@ function App() {
   return (
     <div className="container">
       <h1>Ashok</h1>
-      <div className="clap" onClick={clap}>👏</div>
+
+      <div className="clap" onClick={clap}>
+        👏
+      </div>
+
       <p>Cart Amount: ₹{cart}</p>
     </div>
   );
