eef51644fd81057620c5f714370ab48786c987ed
Ashok-6889 <ashoknelluri2nd@gmail.com>
2026-02-02 01:08:48 +0530

    Update index.js

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import AdminPage from "./AdminPage";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/admin" element={<AdminPage />} />
+    </Routes>
+  </BrowserRouter>
 );
